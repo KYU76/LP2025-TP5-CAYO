2ef38d823b748a2eda3fa0ed8761d9828003b869
Quique <carloscayo76@gmail.com>
2025-06-26 23:23:35 -0300

    feat: se agrega estructura inicial para el TP5: Microservicios - usuarios (puerto 8080) y trámites (puesto 8081)

---
+++ tramites-api/src/test/java/dev/labintec/tramites_api/Tp5CayoTramitesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.labintec.tramites_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5CayoTramitesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ usuarios-api/src/test/java/dev/labintec/usuarios_api/Tp5CayoUsuariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.labintec.usuarios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5CayoUsuariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
